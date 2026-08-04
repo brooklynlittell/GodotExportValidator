94a76aa22bc3035bf3ca6e7572d02371689895a9
Brooklyn Littell <b@blittell.com>
2026-08-04 11:01:03 -0700

    Add test solution

---
+++ GodotExportValidator.Tests/ExportValidatorSnapshotTests.cs
@@ -0,0 +1,32 @@
+﻿using VerifyXunit;
+
+namespace GodotExportValidator.Tests;
+
+public class ExportValidatorSnapshotTests
+{
+    [Fact]
+    public Task GeneratesExportNullValidationCorrectly()
+    {
+        var source = @"
+using Godot;
+using GodotExportValidator;
+
+namespace Tests;
+
+[ExportValidation]
+public partial class GodotComponent : Node3D
+{
+    [Export]
+    [ExportNullCheck]
+    private RayCast3D _rayCast;
+
+    public override void _Ready()
+    {
+        ValidateExports();
+    }
+}
+";
+
+        return TestHelper.Verify(source);
+    }
+}
---
+++ GodotExportValidator.Tests/TestHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace GodotExportValidator.Tests;
+
+public static class TestHelper
+{
+    public static Task Verify(string source)
+    {
+        // Parse and compile code
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "Tests",
+            syntaxTrees: new[] { syntaxTree });
+
+        // Run source generator
+        var generator = new ExportValidatorGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+        driver = driver.RunGenerators(compilation);
+
+        var results = driver.GetRunResult();
+
+        // Test output
+        return Verifier.Verify(results);
+    }
+}
